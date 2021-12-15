95dc27f454a003959a8514d6b51db765c590dff9
STLPhilly <izachariahh@gmail.com>
2021-12-15 17:36:10 -0600

    first commit

---
+++ src/app/orbit-list/orbit-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-list',
+  templateUrl: './orbit-list.component.html',
+  styleUrls: ['./orbit-list.component.css']
+})
+
+export class OrbitListComponent implements OnInit {
+
+  @Input() satellites: Satellite[];
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+	sort(column: string): void {
+		// array.sort modifies the array, sorting the items based on the given compare function
+		this.satellites.sort(function (a: Satellite, b: Satellite): number {
+			if (a[column] < b[column]) {
+				return -1;
+			} else if (a[column] > b[column]) {
+				return 1;
+			}
+			return 0;
+		});
+	}
+
+
+	// getEven(num){
+	// 	num % 2 == 0
+	// 	return;
+	// }
+
+	// loop(){
+	// 	for(let i=0;i<this.satellites.length;i++){
+	// 	if(getEven(this.satellites[i])){
+	// 	}
+	// 	return true;
+	// 	else {
+	// 		return false;
+	// 		}
+	// 	}
+	// }
+}
